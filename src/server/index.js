0dbd89b1ed82b7f2e8441b041b5fdee53d895cad
David Trussler <mail@davidtrussler.net>
2020-10-27 21:30:20 +0000

    Sets up dev and prod servers

--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,9 @@
-// import express from 'express'; 
-
 const express = require('express'); 
 const app = express(); 
 const port = process.env.PORT || 4000; 
 
-// Seve static files
-// app.use(express.static('public')); 
-
-app.get('/', (req, res) => {
-	res.send('Hello World')
-}); 
-
 // Fired every time the server receives a request
-// app.use(handleRender); 
+app.use(handleRender); 
 
 function generateMarkup(req) {
 	return `
@@ -20,66 +11,12 @@ function generateMarkup(req) {
 		<main>main</main>
 		<footer>footer</footer>
 	`
-	// (
-	// 	ReactDOMServer.renderToString(
-	//   	<StaticRouter location={req.url}>
-	// 	  	<App />
-	//   	</StaticRouter>
-	// 	)
-	// )
 }
 
 function handleRender(req, res) {
-  // if (req.url.indexOf('/blog') > -1) {
-		// // Blog page is requested
-		// if (req.url.indexOf('?titleid') == -1) {
-		// 	// Main page requested: display listings
-		// 	dataStore.dispatch({
-		// 		type: 'BLOG_CALLED_MAIN',
-		// 	}); 
+  const markup = generateMarkup(req); 
 
-		// 	fetchAllPosts(data => {
-		// 		// Add response data to store
-		// 		dataStore.dispatch({
-		// 			type: 'POSTS_UPDATED', 
-		// 			payload: data
-		// 		}); 
-
-		// 		const markup = generateMarkup(req); 
-
-		// 		res.send(renderFullPage(markup)); 
-		// 	})
-  // 	} else {
-  // 		const titleid = req.url.split('?titleid=')[1]; 
-  // 		const postidArr = titleid.split('-'); 
-  // 		const postid = postidArr[postidArr.length - 1]; 
-
-		// 	dataStore.dispatch({
-		// 		type: 'BLOG_CALLED_POST', 
-		// 		payload: {
-		// 			'context': 'singlePost', 
-		// 			'postid': postid
-		// 		}
-		// 	}); 
-
-		// 	fetchSinglePost((postid), data => {
-		// 		// Add response data to store
-		// 		dataStore.dispatch({
-		// 			type: 'POST_UPDATED', 
-		// 			postid: postid, 
-		// 			payload: data
-		// 		}); 
-				
-		// 	  const markup = generateMarkup(req); 
-
-		// 		res.send(renderFullPage(markup)); 
-		// 	})
-  // 	}
-  // } else {
-	  const markup = generateMarkup(req); 
-
-		res.send(renderFullPage(markup)); 
-  // }
+	res.send(renderFullPage(markup)); 
 }; 
 
 function renderFullPage(markup) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,8 @@
-const path = require('path'); 
-// const webpack = require('webpack'); 
-// const nodeExternals = require('webpack-node-externals')
-
-// const serverConfig = {
-//   entry: './src/server/index.js',
-//   target: 'node',
-//   externals: [nodeExternals()],
-//   output: {
-//     path: __dirname,
-//     filename: 'server.js',
-//     publicPath: '/'
-//   },
-//   mode: 'production', 
-//   module: {
-//     rules: [
-//       { test: /\.(js)$/, use: 'babel-loader' }, 
-//       { test: /\.(scss)$/, use: ['css-loader', 'sass-loader']}
-//     ]
-//   },
-//   plugins: [
-//     new webpack.DefinePlugin({
-//       __isBrowser__: "false"
-//     })
-//   ]
-// }
-
 module.exports = {
-  entry: './src/server/index.js', 
-  output: {
-    path: __dirname,
-    filename: 'server.js', 
-    publicPath: '/'
-  }
+  entry: './src/server/index.js',
+  mode: 'production', 
+  node: {
+  	fs: 'empty', 
+	  net: 'empty'
+	}
 }
