9708f4d9eeb9386899700975f25bf8978c651b01
David Trussler <mail@davidtrussler.net>
2021-03-09 07:29:36 +0000

    Updates generic `fetchData` method to make query for all reviews

--- src/db/index.js
+++ src/db/index.js
@@ -7,64 +7,76 @@ let client = new Client({
 	connectionString: process.env.DATABASE_URL
 	// Connect to database on Heroku Preview App
 	// connectionString: process.env.HEROKU_POSTGRESQL_<NAME>_URL
-}); 
+});
 
 client.connect(); 
 
-function fetchData() {
-	return new Promise((resolve, reject) => {
-		resolve({
-			// Fetch all reviews from db
-			reviews: [
-				{id: 1, title: 'A thing'},
-				{id: 2, title: 'Another thing'}
-			]
-		});
-	})
+function fetchData(url) {
+	if (url === '/') {
+		let query = 'SELECT id, title, medium FROM reviews ORDER BY timestamp ASC;';
+
+		return new Promise((resolve, reject) => {
+			client.query(query, (err, res) => {
+				if (err) {
+					console.log('error: ', err); 
+					reject(err); 
+				} else {
+					resolve(res.rows);
+				}
+			})
+		}).catch(reject => {
+			console.log('reject: ', reject); 
+			return reject;
+		})
+	} else {
+		return new Promise((resolve, reject) => {
+			resolve(); 
+		})
+	}
 }
 
-function fetchAllPosts(callback) {
-	const query = 'SELECT titleid, timestamp, title FROM posts ORDER BY timestamp DESC;';
+// function fetchAllPosts(callback) {
+// 	const query = 'SELECT titleid, timestamp, title FROM posts ORDER BY timestamp DESC;';
 	
-	return new Promise((resolve, reject) => {
-		client.query(query, (err, res) => {
-			if (err) {
-				reject(err); 
-			} else {
-				resolve(res.rows); 
-			}
-		})
-	}).then((response) => {
-		callback(
-			{posts: response}
-		)
-	}).catch(reject => {
-		return reject;
-	})
-}
+// 	return new Promise((resolve, reject) => {
+// 		client.query(query, (err, res) => {
+// 			if (err) {
+// 				reject(err); 
+// 			} else {
+// 				resolve(res.rows); 
+// 			}
+// 		})
+// 	}).then((response) => {
+// 		callback(
+// 			{posts: response}
+// 		)
+// 	}).catch(reject => {
+// 		return reject;
+// 	})
+// }
 
-function fetchSinglePost(postid, callback) {
-	const query = 'SELECT timestamp, title, body FROM posts WHERE postid=' + postid;
+// function fetchSinglePost(postid, callback) {
+// 	const query = 'SELECT timestamp, title, body FROM posts WHERE postid=' + postid;
 	
-	return new Promise((resolve, reject) => {
-		client.query(query, (err, res) => {
-			if (err) {
-				reject(err); 
-			} else {
-				resolve(res.rows); 
-			}
-		})
-	}).then((response) => {
-		callback(
-			response[0]
-		)
-	}).catch(reject => {
-		return reject;
-	})
-}
+// 	return new Promise((resolve, reject) => {
+// 		client.query(query, (err, res) => {
+// 			if (err) {
+// 				reject(err); 
+// 			} else {
+// 				resolve(res.rows); 
+// 			}
+// 		})
+// 	}).then((response) => {
+// 		callback(
+// 			response[0]
+// 		)
+// 	}).catch(reject => {
+// 		return reject;
+// 	})
+// }
 
 module.exports = {
-  fetchAllPosts, 
-  fetchSinglePost, 
+  // fetchAllPosts, 
+  // fetchSinglePost, 
   fetchData
 }
