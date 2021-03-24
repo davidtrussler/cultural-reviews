8a0b2b60c5212d5cd8cfb19bac422aa569838929
David Trussler <mail@davidtrussler.net>
2021-03-24 23:44:05 +0000

    Adds `assets` directory and configures server for static assets

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const server = express(); 
 const port = process.env.PORT || 4000; 
 
+server.use(express.static('assets')); 
+
 server.get('*', (req, res) => {
 	createSSRApp(req.url).then(app => {
 		renderer.renderToString(app).then(markup => {
