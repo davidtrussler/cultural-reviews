8cd20cf2896836f12f51b782ad1ea838c4c867df
David Trussler <mail@davidtrussler.net>
2021-03-28 13:29:31 +0100

    Adds SQL file for udpated database

---
+++ sql/cultural_reviews_210328.sql
@@ -0,0 +1,116 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.2
+-- Dumped by pg_dump version 13.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: reviews; Type: TABLE; Schema: public; Owner: davidtrussler
+--
+
+CREATE TABLE public.reviews (
+    id smallint NOT NULL,
+    medium character varying(15),
+    title character varying(80),
+    author character varying(30),
+    publisher character varying(30),
+    date_publication date,
+    director character varying(30),
+    actors character varying(100),
+    running_time integer,
+    country character varying(30),
+    date_release date,
+    venue character varying(30),
+    date_opening date,
+    date_closing date,
+    writer character varying(30),
+    date_event date,
+    artist character varying(30),
+    label character varying(30),
+    text text,
+    img_thumb character varying(50),
+    img_main character varying(50),
+    "timestamp" timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.reviews OWNER TO davidtrussler;
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE; Schema: public; Owner: davidtrussler
+--
+
+CREATE SEQUENCE public.reviews_id_seq
+    AS smallint
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.reviews_id_seq OWNER TO davidtrussler;
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: davidtrussler
+--
+
+ALTER SEQUENCE public.reviews_id_seq OWNED BY public.reviews.id;
+
+
+--
+-- Name: reviews id; Type: DEFAULT; Schema: public; Owner: davidtrussler
+--
+
+ALTER TABLE ONLY public.reviews ALTER COLUMN id SET DEFAULT nextval('public.reviews_id_seq'::regclass);
+
+
+--
+-- Data for Name: reviews; Type: TABLE DATA; Schema: public; Owner: davidtrussler
+--
+
+COPY public.reviews (id, medium, title, author, publisher, date_publication, director, actors, running_time, country, date_release, venue, date_opening, date_closing, writer, date_event, artist, label, text, img_thumb, img_main, "timestamp") FROM stdin;
+2	theatre	Death of England	\N	\N	\N	Clint Dyer	Rafe Spall	\N	\N	\N	National Theatre	2020-01-31	2020-03-07	Clint Dyer, Roy Williams	\N	\N	\N	A family in mourning. A man in crisis.	https://picsum.photos/id/1015/60	https://picsum.photos/id/1015/300/200	2021-03-28 11:22:39.234798
+3	cinema	The Loneliness of the Long Distance Runner	\N	\N	\N	Tony Richardson	Michael Redgrave, Tom Courtenay, Avis Bunnage, Alec McCowen	100	UK	1962-01-01	\N	\N	\N	\N	\N	\N	\N	The much-loved Tom Courtenay made his name in this iconic film, the scalding story of teenager Colin Smith, who is sent to a rural borstal for burglary but marked out by his talent as a cross-country runner.	https://picsum.photos/id/1015/60	https://picsum.photos/id/1015/300/200	2021-03-28 11:22:51.066095
+4	art_visual	Anselm Kiefer	\N	\N	\N	\N	\N	\N	\N	\N	White Cube Bermondsey	2019-11-15	2020-01-26	\N	\N	\N	\N	White Cube is pleased to present an exhibition of new work by Anselm Kiefer.	https://picsum.photos/id/1015/60	https://picsum.photos/id/1015/300/200	2021-03-28 11:23:00.643746
+5	music_performed	John Tilbury / Bertrand Gauguet	\N	\N	\N	\N	\N	\N	\N	\N	Cafe Oto	\N	\N	\N	2019-09-18	\N	\N	John Tilbury and Bertrand Gauguet improvise a music of slowness, sometimes silent, sometimes tempestuous. Playing with the acoustic qualities of the place, are woven between the piano and the saxophone subtle games of harmonic textures, appearances of differential sounds, resonances and sometimes some more noisy sequences. A music to stretch the listening like a bow.	https://picsum.photos/id/1015/60	https://picsum.photos/id/1015/300/200	2021-03-28 11:23:11.240502
+6	music_recorded	A Glorious Monster	\N	\N	\N	\N	\N	\N	\N	2018-01-01	\N	\N	\N	\N	\N	Sloth Racket	Luminous	With this fourth album in two years, Cath Roberts Sloth Racket consolidate what has by now become a recognisable MO. Essentially, with Roberts compositions, its all about how the quintet find their way to the inevitable riff.	https://picsum.photos/id/1015/60	https://picsum.photos/id/1015/300/200	2021-03-28 11:23:22.894148
+1	book	Transit	Rachel Cusk	Jonathan Cape	2016-01-01	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	\N	Second part of trilogy that begins with Outline and ends with Kudos	/images/transit_thumb.jpg	/images/Writer-and-novelist-Rache-011.jpg	2021-03-28 11:21:55.922807
+\.
+
+
+--
+-- Name: reviews_id_seq; Type: SEQUENCE SET; Schema: public; Owner: davidtrussler
+--
+
+SELECT pg_catalog.setval('public.reviews_id_seq', 6, true);
+
+
+--
+-- Name: reviews reviews_pkey; Type: CONSTRAINT; Schema: public; Owner: davidtrussler
+--
+
+ALTER TABLE ONLY public.reviews
+    ADD CONSTRAINT reviews_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
