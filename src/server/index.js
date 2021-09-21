9f0487b795f4adee9e4924ddef1b8a569338490c
David Trussler <mail@davidtrussler.net>
2021-09-21 03:04:00 +0100

    Updates server to remove Vue

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,4 @@
-const createSSRApp = require('../app')
-const renderer = require('@vue/server-renderer'); 
+const app = require('../app')
 const express = require('express'); 
 const server = express(); 
 const port = process.env.PORT || 4000; 
@@ -7,16 +6,14 @@ const port = process.env.PORT || 4000;
 server.use(express.static('assets')); 
 
 server.get('*', (req, res) => {
-	createSSRApp(req.url).then(app => {
-		renderer.renderToString(app).then(markup => {
-			res.end(`
-				<!DOCTYPE html>
-				${markup}
-			`);
-		}).catch(err => {
-			console.error(err); 
-		});
-	})
+	app.createSSRApp(req.url).then(markup => {
+		res.end(`
+			<!DOCTYPE html>
+			${markup}
+		`);
+	}).catch(err => {
+		console.error(err);
+	});
 });
 
 server.listen(port, () => {
