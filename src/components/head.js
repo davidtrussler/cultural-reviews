6979acb35766ffcc573fbf508d6d131712b4358e
David Trussler <David.Trussler@mmtdigital.co.uk>
2022-02-17 18:25:30 +0000

    Adds viewport metatag

--- src/components/head.js
+++ src/components/head.js
@@ -18,6 +18,7 @@ const	AppHead = {
 				<title>${title}</title>
 				<meta charset="utf-8"/>
 				<link rel="stylesheet" type="text/css" href="/styles/app.css"/>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 			`
 		)
 	}
