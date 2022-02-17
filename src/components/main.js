f7c007aa6f37bb6b1c4f4d6f3c98bd7ff846b1bc
David Trussler <David.Trussler@mmtdigital.co.uk>
2022-02-17 13:05:43 +0000

    Adds styles for main

--- src/components/main.js
+++ src/components/main.js
@@ -25,16 +25,20 @@ const AppMain = {
 		switch (page) {
 			case 'home':
 				return (
-					`<section>
-						<p>Alright so Bob Dylan wasn&#8217;t impressed with his critics, as expressed in his song <span class="title">Ballad of a Thin Man</span>. Troubadours rarely are. But I&#8217;ve paraphrased his song title to name my reviews site nonetheless because it feels right.</p>
-						<p>I&#8217;m David Trussler and the arts and culture have always been a major part of my life. So I thought I&#8217;d start writing about culture in all its forms. I want to share my enthusiasm for what it can do for us, and occasionally when it maybe doesn&#8217;t quite pull it off.</p>
-						<p>I like to think I have some idea of what&#8217;s going on here.</p>
-					</section>
-					<section>
-						<ul>
-							${getAllReviews(data)}
-						</ul>
-					</section>`
+					`<div class="constrained">
+						<div class="main__inner">
+							<section>
+								<p>Alright so Bob Dylan wasn&#8217;t impressed with his critics, as expressed in his song <span class="title">Ballad of a Thin Man</span>. Troubadours rarely are. But I&#8217;ve paraphrased his song title to name my reviews site nonetheless because it feels right.</p>
+								<p>I&#8217;m David Trussler and the arts and culture have always been a major part of my life. So I thought I&#8217;d start writing about culture in all its forms. I want to share my enthusiasm for what it can do for us, and occasionally when it maybe doesn&#8217;t quite pull it off.</p>
+								<p>I like to think I have some idea of what&#8217;s going on here.</p>
+							</section>
+							<section>
+								<ul>
+									${getAllReviews(data)}
+								</ul>
+							</section>
+						</div>
+					</div>`
 				)
 				break;
 			case 'review':
