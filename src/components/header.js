1c889ccd4e015ec18d21b8a69ee6d87e20e9519a
David Trussler <David.Trussler@mmtdigital.co.uk>
2022-02-17 12:24:35 +0000

    Adds styles for header

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,12 @@ const	AppHeader = {
 	getHtml() {
 		return (
 			`
-				<h1>Thoughts of a thin man</h1>
-				<h2>Some thoughts about art, music, books, cinema and theatre</h2>
+				<div class="constrained">
+					<div class="header__inner">
+						<h1 class="header__title">Thoughts of a thin man</h1>
+						<h2 class="header__subtitle">Some thoughts about art, music, books, cinema and theatre</h2>
+					</div>
+				</div>
 			`
 		)
 	}
