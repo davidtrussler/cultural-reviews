ad8bcb40a556ad2fd86f608d1ed59ede0a67c587
David Trussler <mail@davidtrussler.net>
2022-03-11 23:11:17 +0000

    Update Beta content

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const	AppHeader = {
 				<div class="constrained">
 					<div class="header__inner">
 						<h1 class="header__title">Thoughts of a thin man</h1>
-						<h2 class="header__subtitle">Some thoughts about art, music, books, cinema and theatre</h2>
+						<h2 class="header__subtitle">Some observations about art, music, books, cinema and theatre</h2>
 					</div>
 				</div>
 			`
--- src/components/main.js
+++ src/components/main.js
@@ -28,9 +28,10 @@ const AppMain = {
 					`<div class="constrained">
 						<div class="main__inner">
 							<section class="introduction">
-								<p>Alright so Bob Dylan wasn&#8217;t impressed with his critics, as expressed in his song <span class="title">Ballad of a Thin Man</span>. Troubadours rarely are. But I&#8217;ve paraphrased his song title to name my reviews site nonetheless because it feels right.</p>
-								<p>I&#8217;m David Trussler and the arts and culture have always been a major part of my life. So I thought I&#8217;d start writing about culture in all its forms. I want to share my enthusiasm for what it can do for us, and occasionally when it maybe doesn&#8217;t quite pull it off.</p>
-								<p>I like to think I have some idea of what&#8217;s going on here.</p>
+								<p>Alright so Bob Dylan wasn&#8217;t impressed with critics, as expressed in his song <span class="title">Ballad of a Thin Man</span>. Troubadours rarely are. But I&#8217;ve paraphrased his song title to name my reviews site nonetheless because it feels right.</p>
+								<p>The arts have always been a major part of my life. So I thought I&#8217;d start writing about culture in all its forms. I want to share my enthusiasm for what it can do for us, and occasionally when it maybe doesn&#8217;t quite pull it off.</p>
+								<p>I like to think I have some idea of what&#8217;s happening here.</p>
+								<p>David Trussler, 11 March 2022</p>
 							</section>
 							<section class="reviews-list">
 								<ul>
